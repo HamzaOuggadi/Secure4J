5690be83c53cf510551d5769023aa59d84ae842e
hamzaouggadi <hamza.ouggadi@gmail.com>
2023-03-27 14:28:50 +0000

    Added JwtAuthenticationFilter.java Added JwtService.java

---
+++ src/main/java/net/hamzaouggadi/secure4j/web/AuthenticationController.java
@@ -0,0 +1,12 @@
+package net.hamzaouggadi.secure4j.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthenticationController {
+
+}
