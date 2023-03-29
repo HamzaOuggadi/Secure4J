2370918e85729e3854e6e4e799bafdd361d05c1f
hamzaouggadi <hamza.ouggadi@gmail.com>
2023-03-29 10:45:20 +0000

    Added JwtAuthenticationFilter.java Added JwtService.java

---
+++ src/main/java/net/hamzaouggadi/secure4j/dtos/UserDTO.java
@@ -0,0 +1,14 @@
+package net.hamzaouggadi.secure4j.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String fullName;
+    private String email;
+    private String password;
+}
