c02e92ef013cd545cbd590e6f4efe280bc95a430
hamzaouggadi <hamza.ouggadi@gmail.com>
2023-03-29 11:03:11 +0000

    Added JwtAuthenticationFilter.java Added JwtService.java

---
+++ src/main/java/net/hamzaouggadi/secure4j/entities/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package net.hamzaouggadi.secure4j.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String token;
+}
